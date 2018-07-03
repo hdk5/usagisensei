e31e3f201311c48f08b27cc4e5c975b2d7455876
MakinoHaruka <harukaxxxx@gmail.com>
2018-07-03 11:04:27 +0800

    ship ability bar

--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -5,8 +5,14 @@
       <div v-for="type in tier" :key="type.key">
         <Card v-for="ship in Object.keys(type)" :key="ship.key" :padding="0">
           <div style="text-align:center">
-            <img width="100" height="100" :src="`img/shipicons/${shipData[ship].id}.png`">
-            <p>{{shipData[ship].name}}</p>
+            <div class="ability-container">
+              <div class="ability-placeholder" v-if="!type[ship].ability[0]"/>
+              <div class="ability-bar" :style="`width: ${abilityLength(type[ship].ability)}; background-color: ${abilityColor(ability)};`" v-for="ability in type[ship].ability" :key="ability.key">
+                <p class="ability-text">{{ability}}</p>
+              </div>
+            </div>
+            <img class="ship-image" width="100" height="100" :src="`img/shipicons/${shipData[ship].id}.png`">
+            <p class="ship-name">{{shipData[ship].name}}</p>
           </div>
         </Card>
       </div>
@@ -68,6 +74,39 @@ export default {
     },
     ...mapGetters(['rankData'])
   },
+  methods: {
+    abilityLength: function(ability) {
+      if (ability.length !== 0) {
+        return Math.round(100 / ability.length) + '%'
+      } else {
+        return '100%'
+      }
+    },
+    abilityColor: function(ability) {
+      switch (ability) {
+        case '炮':
+          return '#e00102'
+        case '雷':
+          return '#0000fe'
+        case '弹':
+          return '#00abc9'
+        case '空':
+          return '#028df2'
+        case '爆':
+          return '#e101ad'
+        case '防':
+          return '#ff8b00'
+        case '奶':
+          return '#02b501'
+        case '零':
+          return '#d5b61e'
+        case '辅':
+          return '#fe8100'
+        case '腐':
+          return '#e00003'
+      }
+    }
+  },
   components: {
     isotope
   }
@@ -75,6 +114,9 @@ export default {
 </script>
 
 <style scoped>
+.ivu-layout-content {
+  background: #f5f5f5;
+}
 .rank {
   height: calc(100vh - 60px);
 }
@@ -87,5 +129,50 @@ export default {
   position: absolute;
   bottom: 0;
   text-align: center;
+}
+.ship-image {
+  position: absolute;
+  left: 0;
+  top: 5px;
+}
+.ship-name {
+  position: absolute;
+  top: 106px;
+  margin: 0 auto;
+  width: 100px;
+}
+.ability-bar {
+  height: 5px;
+  display: inline-block;
+  position: relative;
+  top: -2px;
+  z-index: 100;
+  transition: height ease 0.5s;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.6);
+}
+.ability-bar:first-child {
+  border-top-left-radius: 4px;
+}
+.ability-bar:last-child {
+  border-top-right-radius: 4px;
+}
+.ability-bar:hover {
+  height: 20px;
+}
+.ability-placeholder {
+  background: #495060;
+  height: 5px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.6);
+}
+.ability-text {
+  font-size: 12px;
+  opacity: 0;
+  transition: opacity ease 0.6s;
+  color: #fff;
+}
+.ability-text:hover {
+  opacity: 1;
 }
 </style>

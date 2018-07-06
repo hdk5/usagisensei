ef336ce3365caa9172245384886c8723a9ff30d8
MakinoHaruka <harukaxxxx@gmail.com>
2018-07-06 16:22:20 +0800

    page loading bar

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 Vue.use(iView)
 
+// page loading bar
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start()
+  next()
+})
+
+router.afterEach(route => {
+  iView.LoadingBar.finish()
+})
+
+// dynamic title
 Vue.directive('title', {
   inserted: function(el, binding) {
     document.title = '【兎老师】碧蓝航线舰娘强度榜' + binding.value
